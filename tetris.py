7d3b5f0a7b1956895c67611134b5eda50a77dc8f
haukurlogi <haukurlogi2008@gmail.com>
2023-01-15 11:16:51 +0000

    fixed and added

--- tetris.py
+++ tetris.py
@@ -1,38 +1,44 @@
 from microbit import *
 import time
 
+# Lists
 collision_blocks = []
+
+# Integers
 row = 0
 column = 2
-down_speed = 1
+coordinates = int(str(row) + str(column))
+down_speed = 750
+
+# Blocks
+normal_block = display.set_pixel(column, row, 9)
+half_box_block = display.set_pixel(column, row, 9), display.set_pixel(column + 1, row, 9), display.set_pixel(column, row - 1, 9)
+
+# Startup
 display.set_pixel(column, row, 9)
 
-def on_button_pressed_a():
-    global column
-    display.set_pixel(column, row, 0)
-    if coordinates - 10 not in collision_blocks:
-        column -= 1
-    display.set_pixel(column, row, 9)
-button_a(on_button_pressed_a)
-
-def on_button_pressed_b():
-    global column
-    display.set_pixel(column, row, 0)
-    if coordinates + 10 not in collision_blocks:
-        column += 1
-    display.set_pixel(column, row, 9)
-button_b(on_button_pressed_b)
-
 while True:
-    coordinates = int(f"{column}{row}")
-    time.sleep(1)
-    if row < 4 and coordinates + 1 not in collision_blocks:
-            display.set_pixel(column, row, 0)
-    row += 1
-    display.set_pixel(column, row, 9)
+    if button_a.is_pressed():
+        display.set_pixel(column, row, 0)
+        if coordinates - 10 not in collision_blocks:
+            column -= 1
+        display.set_pixel(column, row, 9)
 
-    if row == 5 or coordinates + 1 in collision_blocks:
-        collision_blocks.insert(coordinates)
+    if button_b.is_pressed():
+        display.set_pixel(column, row, 0)
+        if coordinates + 10 not in collision_blocks:
+            column += 1
+        display.set_pixel(column, row, 9)
+
+    time.sleep_ms(down_speed)
+
+    if row < 4 and coordinates + 1 not in collision_blocks:
+        display.set_pixel(column, row, 0)
+        row += 1
+        display.set_pixel(column, row, 9)
+
+    if row == 4 or coordinates + 1 in collision_blocks:
+        collision_blocks.append(coordinates)
         row = 0
         column = 2
         display.set_pixel(column, row, 9)

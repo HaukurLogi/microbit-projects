d0f4e5dd30bd03994ad01154c1e7f49b82e2f1d8
haukurlogi <haukurlogi2008@gmail.com>
2023-03-01 14:25:02 +0000

    added drawing.py

--- tetris.py
+++ tetris.py
@@ -1,51 +1,44 @@
 from microbit import *
-from random import randint
+import time
 
 # Lists
-
 collision_blocks = []
-active_pixels = []
 
 # Integers
 row = 0
 column = 2
-pixel_x = 0
-pixel_y = 0
-random_block = randint(1, 3)
-down_speed = 750
+wait_speed = 750
 
-
-# Blocks
-def blocks(light_level):
-    global row, column, random_block, coordinates
+# Functions
+def summon(light_level):
     display.set_pixel(column, row, light_level)
-    
+
 # Startup
-blocks(9)
+display.set_pixel(column, row, 9)
 
 while True:
-    coordinates = int(str(row) + str(column))
-    if button_a.was_pressed():
-        blocks(0)
+    coordinates = int(str(column) + str(row))
+
+    if button_a.is_pressed():
+        summon(0)
         if coordinates - 10 not in collision_blocks:
             column -= 1
-        blocks(9)
+        summon(9)
 
-    if button_b.was_pressed():
-        blocks(0)
+    if button_b.is_pressed():
+        summon(0)
         if coordinates + 10 not in collision_blocks:
             column += 1
-        blocks(9)
+        summon(9)
 
-    sleep(down_speed) 
-
+    sleep(wait_speed)
+    
     if row < 4 and coordinates + 1 not in collision_blocks:
-        blocks(0)
-        row += 1 
-        blocks(9)
-    if row >= 4 or coordinates + 1 in collision_blocks:
-        collision_blocks.append(active_pixels)
+        summon(0)
+        row += 1
+        summon(9)
+    else:
+        collision_blocks.append(coordinates)
         row = 0
         column = 2
-        random_block = randint(1, 3)
-        blocks(9)
+        summon(9)
